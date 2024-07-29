0f70e5cb467afcb6773ced6ed2b672f793200475
lucassalzotto-dat <lucassalzotto@gmail.com>
2024-07-29 09:35:34 -0300

    First Commit

---
+++ product/repositories/category.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from product.models import Category
+
+class CategoryRepository:
+
+    def get_all(self)-> List[Category]:
+        return Category.objects.all()
---
+++ product/templates/index/raiz.py
@@ -0,0 +1,4 @@
+def raiz_cuadrada(numero):
+    return numero * 0.5
+
+print(raiz_cuadrada(5))
